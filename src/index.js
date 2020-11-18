d5b6538f26c942dbc87e8d9278507dc5875ef828
Ranjan-Das-cloud <ranjan.rd08@gmail.com>
2020-11-18 11:43:19 +0530

    Added index.js file to this repo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import 'bootstrap/dist/css/bootstrap.css';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import App from './app';
+
+import './style.css';
+
+//let element = React.createElement('div',null,'Hello World');
+
+/*let element = React.createElement(
+    'div',
+    null,
+    React.createElement('h1',
+    null,
+    React.createElement('span',null,'Hello World')
+    )
+    );*/
+
+    // Using JSX we can solve the problem of writing multiple times the same React.createElement()
+
+    //let element = <div><h1><span>Hello World</span></h1></div>;
+
+    // Behind the scenes JSX performs operations of building React Component by creating Virtual DOMs and pushing them into the React DOM internally each and every time when it collides wwith each and every HTML tags.
+
+ReactDOM.render(<App/>,document.querySelector('#root'));
+//ReactDOM.render(<App/>,document.getElementById("root"));
+
+
+
+
+
+
